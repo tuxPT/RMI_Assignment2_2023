7654dbd8f580ec100d8c61f0f028f2694f6e9e44
guilhermecsousa <guilhermesousa@ua.pt>
2022-09-27 17:08:26 +0100

    challenge 1

--- pClient/mainRob.py
+++ pClient/mainRob.py
@@ -47,7 +47,7 @@ class MyRob(CRobLinkAngs):
                     state='wait'
                 if self.measures.ground==0:
                     self.setVisitingLed(True);
-                self.wander()
+                self.goC1()
             elif state=='wait':
                 self.setReturningLed(True)
                 if self.measures.visitingLed==True:
@@ -60,13 +60,10 @@ class MyRob(CRobLinkAngs):
                     self.setVisitingLed(False)
                 if self.measures.returningLed==True:
                     self.setReturningLed(False)
-                self.wander()
+                self.goC1()
             
 
     def wander(self):
-        self.wanderC1()
-        
-        """ 
         center_id = 0
         left_id = 1
         right_id = 2
@@ -85,30 +82,27 @@ class MyRob(CRobLinkAngs):
             self.driveMotors(0.0,0.1)
         else:
             print('Go')
-            self.driveMotors(0.1,0.1) 
-        """
+            self.driveMotors(0.1,0.1)
 
 
-    def wanderC1(self):
-        if self.measures.lineSensor == ['0','0','1','1','1','0','0']:
+    def goC1(self):
+        print(self.measures.lineSensor)
+        if self.measures.lineSensor[0] == '1':
+            print('Rotate left')
+            self.driveMotors(-0.12,0.15)
+        elif self.measures.lineSensor[6] == '1':
+            print('Rotate right')
+            self.driveMotors(0.15,-0.12)
+        elif self.measures.lineSensor[1] == '1':
+            print('Adjust left')
+            self.driveMotors(0.08,0.15)
+        elif self.measures.lineSensor[5] == '1':
+            print('Adjust right')
+            self.driveMotors(0.15,0.08)
+        elif '1' in self.measures.lineSensor[2:5]:
             print('Go')
             self.driveMotors(0.15, 0.15)
-        elif '1' in self.measures.lineSensor[:-5]:
-            print('Rotate left')
-            self.driveMotors(0.0,0.05)
-        elif '1' in self.measures.lineSensor[5:]:
-            print('Rotate right')
-            self.driveMotors(0.05,0.0)
-        elif self.measures.lineSensor[2] == '0':
-            print('Rotate slowly left')
-            self.driveMotors(0.01,0.15)
-        elif self.measures.lineSensor[4] == '0':
-            print('Rotate slowly right')
-            self.driveMotors(0.15,0.01)
-        else:
-            print('Stop')
-            print(self.measures.lineSensor)
-            self.driveMotors(0.0, 0.0)
+ 
 
 class Map():
     def __init__(self, filename):
